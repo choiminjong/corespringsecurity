b7aa994d280a8538649d8d4ca174602bbcea3e55
alswhd20 <alswhd20@gmail.com>
2022-01-02 10:01:27 +0900

    Initial commit

---
+++ src/main/java/io/corespringsecurity/CorespringsecurityApplication.java
@@ -0,0 +1,14 @@
+package io.corespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+	public class CorespringsecurityApplication {
+	public static void main(String[] args) {
+
+		SpringApplication.run(CorespringsecurityApplication.class, args);
+	}
+
+}
