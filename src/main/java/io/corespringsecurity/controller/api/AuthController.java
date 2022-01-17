0ce57f6da985788820ac9a6060ac3fb45036f4c1
alswhd20 <alswhd20@gmail.com>
2022-01-17 20:06:32 +0900

    jwt -auth 컨트롤에서 데이터 타입 맞춰서 리턴하는 부분 1차 성공

--- src/main/java/io/corespringsecurity/controller/api/AuthController.java
+++ src/main/java/io/corespringsecurity/controller/api/AuthController.java
@@ -23,86 +23,24 @@ import javax.validation.Valid;
 
 @RestController
 public class AuthController {
-    private final TokenProvider tokenProvider;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    @Autowired
+    private TokenProvider tokenProvider;
 
     @Autowired
     private UserDetailsService userDetailsService;
 
-    public AuthController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
-        this.tokenProvider = tokenProvider;
-        this.authenticationManagerBuilder = authenticationManagerBuilder;
-    }
-
-//    @PostMapping("/api/2")
-//    public String authorize2() {
-//        return "messages OK";
-//    }
-
-    @GetMapping("/api/check")
-    public String authorize2(){
-        return "messages OK";
-    }
-
-    @PostMapping("/api/check2")
-    public String authorize3(){
-        return "messages OK";
-    }
-
     @PostMapping("/api/authenticate")
     public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
-        String jwt="sdsdsd";
-        System.out.println("loginDto getUsername = " + loginDto.getUsername());
-        System.out.println("loginDto getPassword = " + loginDto.getPassword());
-
-//        UsernamePasswordAuthenticationToken authenticationToken =
-//                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
 
         AccountContext accountContext =(AccountContext)userDetailsService.loadUserByUsername(loginDto.getUsername());
         System.out.println("accountContext = " + accountContext);
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null,accountContext.getAuthorities());
-
         System.out.println("authentication ==" + authenticationToken);
-
-
-        String jwt2 = tokenProvider.createToken(authenticationToken);
-
-        System.out.println("jwt2 ==" + jwt2);
-//        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-////
-////        System.out.println("authentication ==" + authentication);
-
-        //String jwt = tokenProvider.createToken(authentication);
-
-        //Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        //SecurityContextHolder.getContext().setAuthentication(authentication);
-
-
-        //System.out.println("authentication = " + authentication);
-        //loginDto.getUsername(), loginDto.getPassword()
-//        UsernamePasswordAuthenticationToken authenticationToken =
-//                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
-
-//        String username = loginDto.getUsername();
-//        String password = loginDto.getPassword();
-//
-//        AccountContext accountContext =(AccountContext)userDetailsService.loadUserByUsername(username);
-//
-//        //토큰생성
-//        UsernamePasswordAuthenticationToken authenticationToken =
-//                new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null,accountContext.getAuthorities());
-//
-//        //객체를 생성
-//        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-//        System.out.println("authenticationManagerBuilder = " + authentication);
-//
-//        //토큰생성
-//        String jwt = tokenProvider.createToken(authentication);
+        String jwt = tokenProvider.createToken(authenticationToken);
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
-
         return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
     }
 }
